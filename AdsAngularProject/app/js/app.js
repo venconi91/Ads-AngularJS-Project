3f20948e6610f3b23e97abaf932b987b23d85187
venconi91 <vencislav_911115@abv.bg>
2014-12-27 23:35:53 +0200

    structure created

---
+++ AdsAngularProject/app/js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('softUniApp', ['ngResource', 'ngRoute'])
+.config(function ($routeProvider, $locationProvider) {
+	$routeProvider.when('/newAd', {
+		templateUrl: 'templates/newAd.html',
+		controller: 'NewAdController'
+	});
+	$routeProvider.when('/ads', {
+		templateUrl: 'templates/allAds.html',
+		controller: 'AllAdsController'
+	});
+	$routeProvider.when('/ads/:adId', {
+		templateUrl: 'templates/adDetails.html',
+		controller: 'AdDetailsController'
+	});
+	$routeProvider.when('/login', {
+	    //templateUrl: 'templates/login.html',
+	    controller: 'LoginController'
+	});
+	$routeProvider.when('/register', {
+	    //templateUrl: 'templates/register.html',
+	    controller: 'RegisterController'
+	});
+	//$locationProvider.html5Mode(true);
+});
